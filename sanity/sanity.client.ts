8f73b514516877bedda50b78784ce4e274fabae7
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-22 12:20:31 +0800

    env implement

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,9 +1,8 @@
 import { createClient, type ClientConfig } from "@sanity/client";
 
 const config: ClientConfig = {
-//   projectId: "ga8lllhf",
-  projectId: "gwieubfy",
-  dataset: "production",
+  projectId: process.env.SANITY_PROJECT_ID || "1x2z2j5o",
+  dataset: process.env.SANITY_DATASET || "production",
   apiVersion: "2023-11-21",
   useCdn: false,
 };
